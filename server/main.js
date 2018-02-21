094b4b61bdc770ab747c948a831625e4462666e5
Luciano Pestana <lucianopestana@hotmail.com>
2018-02-20 22:46:41 -0300

    Meteor-React 10

---
+++ server/main.js
@@ -0,0 +1,8 @@
+// import './../imports/utils';
+
+import {greetUser} from './../imports/utils';
+import {add} from './../imports/math';
+
+console.log('Log form /server/main.js');
+console.log(greetUser());
+console.log(add(5, 7));
