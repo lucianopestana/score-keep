e241428fc3dbb6b91c54c9584a1b5fcdaa8c9d2d
Luciano Pestana <lucianopestana@hotmail.com>
2018-02-20 23:59:37 -0300

    Score-keep - Aula 11

--- imports/utils.js
+++ imports/utils.js
@@ -5,3 +5,6 @@ export let greetUser = function () {
 };
 
 export let name = 'Luciano Pestana';
+
+let myDefault = 'Some default value';
+export default myDefault;
--- server/main.js
+++ server/main.js
@@ -1,8 +1,8 @@
 // import './../imports/utils';
 
 import {greetUser} from './../imports/utils';
-import {add} from './../imports/math';
+import add from './../imports/math';
 
 console.log('Log form /server/main.js');
 console.log(greetUser());
-console.log(add(5, 7));
+console.log(add(8, 7));
